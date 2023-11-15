6baf07c31da7754abd467da93f5ec1fef7d12306
Geetika <geet1kanwar@gmail.com>
2023-11-15 23:17:53 +0530

    updated the id-navbar clicks

--- script.js
+++ script.js
@@ -22,3 +22,26 @@ gsap.to("#main", {
         scrub: 200
     },
 },)
+
+// scroll to id
+document.addEventListener("DOMContentLoaded", function () {
+    var navOptions = document.querySelectorAll('.nav-option');
+    var navbarHeight = document.getElementById('nav').offsetHeight; // Get the height of the navbar
+
+    navOptions.forEach(function (option) {
+        option.addEventListener('click', function () {
+            var targetId = option.getAttribute('data-target');
+            var targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                var offset = 100; // Adjust this value as needed
+                var targetPosition = targetElement.offsetTop - navbarHeight - offset;
+
+                window.scrollTo({
+                    top: targetPosition,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+});
